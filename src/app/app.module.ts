6cbc9e7ea5878ea9337204318975e305549e0d2e
Claudia <claudiabdm@hotmail.es>
2020-05-13 18:02:16 +0200

    Create header and player component and modify app template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { PlayerComponent } from './components/player/player.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    PlayerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
