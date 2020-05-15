3bb48095193b24fc7a8363995d8ce7a2d783790a
Claudia <claudiabdm@hotmail.es>
2020-05-16 00:50:29 +0200

    Add artist info component

---
+++ src/app/components/artist/artist-info/artist-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artist-info',
+  templateUrl: './artist-info.component.html',
+  styleUrls: ['./artist-info.component.scss']
+})
+export class ArtistInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
