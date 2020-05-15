a6ebba1346a22d46656768350ac23b673c817126
Claudia <claudiabdm@hotmail.es>
2020-05-16 01:03:45 +0200

    Add methods to get artist related and artist tracks

--- src/app/services/spotify-api.service.ts
+++ src/app/services/spotify-api.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { map, scan } from 'rxjs/operators';
+import { map, scan, tap, switchMap } from 'rxjs/operators';
+import { Observable, of, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,12 @@ import { map, scan } from 'rxjs/operators';
 export class SpotifyApiService {
 
   url = environment.spotifyApiUrl;
+  currentArtistId: string = '3AA28KZvwAUcZuOKwyblJQ';
+  currentArtist$: Observable<Object>;
+  currentArtistAlbums$: Observable<Object>;
+  currentArtistRelated$: Observable<Object>;
+  currentArtistTracks$: Observable<Object>;
+
 
   constructor(
     private http: HttpClient
@@ -19,7 +26,7 @@ export class SpotifyApiService {
   }
 
   getArtistAlbums(id: string) {
-    return this.http.get(`${this.url}${id}/albums`)
+   return this.http.get(`${this.url}${id}/albums?include_groups=album&market=ES`)
       .pipe(
         map((res: any) => res.items
           .filter((item, idx: number) => {
@@ -29,8 +36,16 @@ export class SpotifyApiService {
             return item.name !== res.items[idx + 1].name;
           })
           .sort((a, b) => new Date(a.release_date) > new Date(b.release_date))
-          )
+        )
       );
+  }
+
+  getArtistRelated(id: string) {
+    return this.http.get(`${this.url}${id}/related-artists`).pipe(map((res: any) => res.artists));
+  }
+
+  getArtistTracks(id: string) {
+    return this.http.get(`${this.url}${id}/top-tracks?country=ES`).pipe(map((res: any) => res.tracks));
   }
 
 }
