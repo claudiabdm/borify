bfd148e7459218b7a53be3e0fa73464cacfcc8ea
Claudia <claudiabdm@hotmail.es>
2020-05-16 00:58:25 +0200

    Remove files

--- src/app/interceptors/spotify-api.interceptor.ts
+++ src/app/interceptors/spotify-api.interceptor.ts
@@ -1,20 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpClient,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { SpotifyApiService } from '../services/spotify-api.service';
+import { tap } from 'rxjs/internal/operators/tap';
+import { catchError, switchMap, flatMap, map, retry } from 'rxjs/operators';
+import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
 
 @Injectable()
 export class SpotifyApiInterceptor implements HttpInterceptor {
 
-  constructor() { }
+  constructor(
+    private spotifyApi: SpotifyApiService,
+    private http: HttpClient,
+    @Inject(LOCAL_STORAGE) private storage: StorageService
+  ) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const authReq = request.clone({ setHeaders: { Authorization: environment.spotifyApiToken } });
-    return next.handle(authReq);
+    const token = 'Bearer BQDwn1OU5jIinC-J2HtYIpr-hjFdcZ4uz94EGavMSrXJil024vai3WsNZCMnRc-k8ia4AuvqHiQFGkb7VE8';
+    return next.handle(this.addToken(request, token))
+  }
+
+  private addToken(request: HttpRequest<unknown>, token: string) {
+    return request.clone({ setHeaders: { Authorization: token } });
   }
 }
