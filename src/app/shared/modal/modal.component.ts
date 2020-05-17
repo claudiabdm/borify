76b9140323d0a4bb5f9331d4a5dee70433f318c5
Claudia <claudiabdm@hotmail.es>
2020-05-17 18:24:18 +0200

    Add logic to create the playlist

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { PlaylistsService } from 'src/app/services/playlists.service';
+import { SpotifyApiService } from 'src/app/services/spotify-api.service';
 
 @Component({
   selector: 'app-modal',
@@ -12,16 +13,23 @@ export class ModalComponent implements OnInit {
 
 
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private playlistService: PlaylistsService) { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private playlistService: PlaylistsService,
+    private spotifyApi: SpotifyApiService
+  ) { }
 
   ngOnInit(): void {
+
   }
 
-  sendPlayList(input) {
-    this.playlistService.addPlayList(input);
+  async createPlayList(input) {
+    const track = await this.spotifyApi.getTrack(this.playlistService.selectedSongId).toPromise();
+    const newPlaylist = {
+      name: input,
+      tracks: [track],
+    }
+    this.playlistService.addPlayList(newPlaylist);
   }
-
-
-
 
 }
