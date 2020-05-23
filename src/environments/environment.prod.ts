2619e2e479c402a95bd367b93d7546f44c1984d2
Claudia <claudiabdm@hotmail.es>
2020-05-24 01:47:39 +0200

    Fix api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   spotifyApiBasicToken: 'Basic MjA4NDNlYTJlY2M0NDk4Y2I4YjFkNjkyNTM1ZTBjMjI6MGE0YjE4NGQzYjdmNDQxYTljZjA1ZDAwZTgzY2JhNmI',
-  spotifyApiUrl: "https://api.spotify.com/v1/",
+  spotifyApiUrl: "https://api.spotify.com/v1",
 };
